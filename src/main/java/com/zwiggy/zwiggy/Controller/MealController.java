0c7cc1e99193e100997d239078a5c5caf3b10118
ansh-parnami <ansh.parnami1@s.amity.edu>
2025-06-13 17:47:57 +0530

    database hosted

--- src/main/java/com/zwiggy/zwiggy/Controller/MealController.java
+++ src/main/java/com/zwiggy/zwiggy/Controller/MealController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RequestMapping("/meals")
 public class MealController {
     @Autowired
